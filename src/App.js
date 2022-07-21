6bf37f21824a365ed95ef6d2f9170be37737c233
dharatipokiya22 <dharatipokiya22@gmail.com>
2022-07-21 15:51:20 +0530

    -first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+import "./App.css";
+import React from "react";
+import {Routes,Route} from 'react-router-dom'
+import Home from "./components/pages/Home";
+import View from "./components/student/View"
+import Edit from "./components/student/Edit"
+
+
+function App()
+{
+ 
+  return(
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/view/:id" element={<View/>}/>
+        <Route path="/edit/:id" element={<Edit/>}/>
+      </Routes>
+      
+    
     </div>
-  );
+  ); 
 }
+
+
+
 
 export default App;
